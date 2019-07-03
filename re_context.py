67cfa03167dd1d8575764c20b7bcdf9d2eff778b
Robin 'r0w' Weiland <robinweiland@gmx.de>
2019-07-04 00:28:34 +0200

    added re_context.py with class stubs and docstrings

---
+++ re_context.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Robin "r0w" Weiland'
+__credits__ = ('Robin Weiland',)
+__copyright__ = 'Copyright 2019, Robin Weiland'
+
+__date__ = '2019-07-03'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
+__all__ = ('Pattern',)
+
+"""utility for creating regex-patterns with a context manager"""
+
+
+class Pattern:
+    """RegEx pattern helper class"""
+
+
+if __name__ == '__main__': pass
